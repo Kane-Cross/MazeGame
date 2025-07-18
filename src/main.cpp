9264c1f83e74bed00cdc2b6723ed1d95a443efa7
Kane-Cross <69941408+Kane-Cross@users.noreply.github.com>
2025-07-18 15:28:36 +0100

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,17 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
 
-#include "g.hpp"
-#include "bin folder/k.hpp"
-
 int main(int argc, const char* argv[]){
-    log("Test message");
+    SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+    IMG_Init(IMG_INIT_PNG);
+    Mix_Init(MIX_INIT_MP3);
+
+    //Some code here
+
+    Mix_Quit();
+    IMG_Quit();
+    TTF_Quit();
+    SDL_Quit();
     return 0;
 }
