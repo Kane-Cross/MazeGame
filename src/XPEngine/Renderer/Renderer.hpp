8635c85c8e9471a6a0b9bc402c9acdd0d219f635
Kane Cross <kanegcross@gmail.com>
2025-07-19 20:57:45 +0100

    Ingore vscode folder - updates to renderer

--- src/XPEngine/Renderer/Renderer.hpp
+++ src/XPEngine/Renderer/Renderer.hpp
@@ -2,17 +2,24 @@
 
 #include "../external/glad/include/glad/glad.h"
 #include <GLFW/glfw3.h>
+#include <vector>
 
 #include "../Window/Window.hpp"
 
 namespace XPE{
     class Renderer{
         Window* parent_window;
+        std::vector<float> vertices;
+        unsigned int VAO;
+        std::vector<int> indices;
+        unsigned int EBO;
+        std::vector<unsigned int> shaders;
     public:
         Renderer();
         Renderer(Window* window);
         bool CreateRenderer(Window* window);
         void SetViewport(int x, int y, int width, int height);
+        void DrawRectangle();
         bool IsValid();
         void Destroy();
     };
